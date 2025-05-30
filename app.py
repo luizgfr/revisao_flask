ca3daee834dfe95af248f42d5a7de7fda58cd824
Luiz Guilherme <luiz.gfr2002@gmail.com>
2025-05-29 21:08:48 -0300

    Iniciando projeto

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='views')
+
+@app.route('/') #decorator '@' atribui uma nova funcionalidade a função que está abaixo dele
+def home():
+    return render_template('index.html')
+
+@app.route('/games')
+def games():
+    game = {
+        'Titulo' : 'CS-GO',
+        'Ano' : 2012,
+        'Categoria' : 'FPS Online'
+    }
+    jogadores = ['Pedro', 'João', 'Marcos', 'Maria', 'Diego']
+    return render_template('games.html', game=game, jogadores=jogadores)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug = True)
+    
+    
+    
